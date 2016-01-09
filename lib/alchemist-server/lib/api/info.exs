b8ac84b52f40e364f4fa6473cb1c409890a29514
Marlus Saraiva <marlus.saraiva@gmail.com>
2016-01-09 20:36:44 -0300

    Merging changes from alchemist-server

--- lib/alchemist-server/lib/api/info.exs
+++ lib/alchemist-server/lib/api/info.exs
@@ -5,6 +5,8 @@ defmodule Alchemist.API.Info do
 
   @moduledoc false
 
+  import IEx.Helpers, warn: false
+
   alias Alchemist.Helpers.ModuleInfo
   alias Alchemist.Helpers.Complete
 
@@ -24,7 +26,7 @@ defmodule Alchemist.API.Info do
 
     modules ++ functions
     |> Enum.uniq
-    |> Enum.map &IO.puts/1
+    |> Enum.map(&IO.puts/1)
 
     IO.puts "END-OF-INFO"
   end
@@ -37,13 +39,50 @@ defmodule Alchemist.API.Info do
     |> Mix.Task.load_tasks
     |> Enum.map(&Mix.Task.task_name/1)
     |> Enum.sort
-    |> Enum.map &IO.puts/1
+    |> Enum.map(&IO.puts/1)
 
     IO.puts "END-OF-INFO"
   end
 
+  def process({:info, arg}) do
+    try do
+      Code.eval_string("i(#{arg})", [], __ENV__)
+    rescue
+      _e -> nil
+    end
+
+    IO.puts "END-OF-INFO"
+  end
+
+  def process({:types, arg}) do
+    try do
+      Code.eval_string("t(#{arg})", [], __ENV__)
+    rescue
+      _e -> nil
+    end
+
+    IO.puts "END-OF-INFO"
+  end
+
+  def process(nil) do
+    IO.puts "END-OF-INFO"
+  end
+
   def normalize(request) do
-    {{_, name }, _} = Code.eval_string(request)
-    name
+    try do
+      arguments = Code.eval_string(request)
+      case arguments do
+        {{_, type }, _}     -> type
+        {{_, type, arg}, _} ->
+          IO.puts System.version
+          if Version.match?(System.version, ">=1.2.0-rc.0") do
+            {type, arg}
+          else
+            nil
+          end
+      end
+    rescue
+      _e -> nil
+    end
   end
 end
