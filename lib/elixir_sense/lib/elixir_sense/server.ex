4534a1c33fb5f2134e59d30eaf91f279e142872c
Marlus Saraiva <marlus.saraiva@gmail.com>
2017-03-28 21:53:16 -0300

    Update ElixirSense

--- lib/elixir_sense/lib/elixir_sense/server.ex
+++ lib/elixir_sense/lib/elixir_sense/server.ex
@@ -1,10 +1,24 @@
 defmodule ElixirSense.Server do
 
-  def start([socket_type, port, env]) do
+  def start(args) do
+    [socket_type, port, env] = validate_args(args)
     IO.puts(:stderr, "Initializing ElixirSense server for environment \"#{env}\" (Elixir version #{System.version})")
     IO.puts(:stderr, "Working directory is \"#{Path.expand(".")}\"")
     ElixirSense.Server.TCPServer.start([socket_type: socket_type, port: port, env: env])
     loop()
+  end
+
+  defp validate_args(["unix", _port, env] = args) do
+    {version, _} = :erlang.system_info(:otp_release) |> :string.to_integer
+    if version < 19 do
+      IO.puts(:stderr, "Warning: Erlang version < 19. Cannot use Unix domain sockets. Using tcp/ip instead.")
+      ["tcpip", "0", env]
+    else
+      args
+    end
+  end
+  defp validate_args(args) do
+    args
   end
 
   defp loop() do
